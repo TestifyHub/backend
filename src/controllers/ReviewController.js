d4ac2697d98c8c166eb40f6c04428a94f7f415d8
Nilay Shah <nilayshah630@gmail.com>
2024-08-27 14:46:34 +0530

    reviews

--- src/controllers/ReviewController.js
+++ src/controllers/ReviewController.js
@@ -54,4 +54,77 @@ const addVideoReview = async (req, res) => {
   }
 };
 
-module.exports = { addTextReview, addVideoReview };
+const getReviewsBySpaceId = async (req, res) => {
+  try {
+    const { spaceId } = req.params;
+    const reviews = await reviewSchema.find({ spaceId });
+    res.status(200).json({ success: true, reviews });
+  } catch (err) {
+    console.error("Error fetching reviews:", err);
+    res.status(500).json({ message: "Internal server error", err });
+  }
+};
+
+const getLikedReviewsBySpaceId = async (req, res) => {
+  try {
+    const { spaceId } = req.params;
+    const reviews = await reviewSchema.find({ spaceId, liked: true });
+    res.status(200).json({ success: true, reviews });
+  } catch (err) {
+    console.error("Error fetching liked reviews:", err);
+    res.status(500).json({ message: "Internal server error", err });
+  }
+};
+
+const getTextReviewsBySpaceId = async (req, res) => {
+  try {
+    const { spaceId } = req.params;
+    const reviews = await reviewSchema.find({ spaceId, type: "text" });
+    res.status(200).json({ success: true, reviews });
+  } catch (err) {
+    console.error("Error fetching text reviews:", err);
+    res.status(500).json({ message: "Internal server error", err });
+  }
+};
+
+const getVideoReviewsBySpaceId = async (req, res) => {
+  try {
+    const { spaceId } = req.params;
+    const reviews = await reviewSchema.find({ spaceId, type: "video" });
+    res.status(200).json({ success: true, reviews });
+  } catch (err) {
+    console.error("Error fetching video reviews:", err);
+    res.status(500).json({ message: "Internal server error", err });
+  }
+};
+
+const likeReview = async (req, res) => {
+  const { reviewId } = req.params;
+
+  try {
+    
+    const review = await Review.findById(reviewId);
+    if (!review) {
+      return res.status(404).json({ message: 'Review not found' });
+    }
+
+    
+    review.liked = !review.liked;
+    await review.save();
+
+    res.json(review);
+  } catch (error) {
+    console.error("Error liking review:", error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = {
+  addTextReview,
+  addVideoReview,
+  getReviewsBySpaceId,
+  getLikedReviewsBySpaceId,
+  getTextReviewsBySpaceId,
+  getVideoReviewsBySpaceId,
+  likeReview,
+};
